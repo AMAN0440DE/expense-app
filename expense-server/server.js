744588e38e07fc38065d2cb74a5c5c6a1bfe9408
AMAN0440DE <kumar02aman2004@gmail.com>
2026-01-23 10:57:07 +0530

    updated

---
+++ expense-server/server.js
@@ -0,0 +1,16 @@
+console.log("Hi, I'm your server!");
+
+/**
+ * PS C:\Users\kumar_1sb3con\Desktop\Expense-app\expense-server> npm run start
+
+> expense-server@1.0.0 start
+> node server.js
+
+Hi, I'm your server!
+PS C:\Users\kumar_1sb3con\Desktop\Expense-app\expense-server> node server.js
+Hi, I'm your server!
+
+will giuve the same output
+as we added the start command (custom command) in package.json file
+either we can run npm run start or node server.js to run the server
+ */
