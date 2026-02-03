54f81c92422b56e0504d28c9848071f6b67c5f8e
AMAN0440DE <kumar02aman2004@gmail.com>
2026-02-03 18:43:11 +0530

    added membersgroup

--- expense-server/src/routes/groupRoutes.js
+++ expense-server/src/routes/groupRoutes.js
@@ -7,10 +7,11 @@ const router = express.Router();
 router.use(authMiddleware.protect);  // protect all group routes
 router.post('/create', groupController.create);
 router.put('/:groupId', groupController.updateGroup);
-router.post('/:groupId/members', groupController.addMembers);
+router.patch('/:groupId/members', groupController.addMembers);
 router.delete('/:groupId/members', groupController.removeMembers);
-router.get('/email/:email', groupController.getGroupByEmail);
-router.get('/status/:status', groupController.getGroupByStatus);
+router.get('/my-groups', groupController.getGroupsByUser);
+router.get('/status/:status', groupController.getGroupsByPaymentStatus);
 router.get('/:groupId/audit-log', groupController.getAuditLog);
+router.get('/email/:email', groupController.getGroupByEmail);
 
 module.exports = router;
