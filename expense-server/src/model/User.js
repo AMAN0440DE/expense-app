7a0648bc5934d869bc0f063a2f4bd598e3a9a4bf
AMAN0440DE <kumar02aman2004@gmail.com>
2026-02-05 23:17:38 +0530

    updated user.js

--- expense-server/src/model/User.js
+++ expense-server/src/model/User.js
@@ -4,10 +4,13 @@ const userSchema = new mongoose.Schema({             // creates schema from .Sch
     name: { type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: false},
-    googleId: {type: String, required: false}
+    googleId: {type: String, required: false},
+    role: {type: String, required: true},
+    adminId: {type: mongoose.Schema.Types.ObjectId, ref: "User" , index: true},
 });
 
 module.exports = mongoose.model('User', userSchema);   // creates model from schema and exports it so that other files can use it
 
 // privacy policy defines how website handles data and if voilated user may file a case on the website for revealing sensitive data
 // / for now store in plin text and apply encryption
+
